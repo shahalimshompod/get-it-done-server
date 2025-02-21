7b442789995bfc3dbb6273dd7b6aae57a971af76
shahalimshompod <shahalimsompod@gmail.com>
2025-02-21 10:50:37 +0600

    added api for adding registered/loggedin users to database

--- index.js
+++ index.js
@@ -8,11 +8,66 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongodb
 
-app.get('/', (req, res) => {
-    res.send("Get It Done is running");
-})
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ezm1s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+
+    // database and collections
+    const userCollection = client.db("get-it-done").collection("users");
+    const tasksCollection = client.db("get-it-done").collection("tasks");
+
+    // OPERATIONS
+
+    // POST OPERATIONS
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "USER ALREADY EXISTS", insertedId: null });
+      }
+
+      const userToBeAdded = {
+        ...user,
+        createdAt: new Date(),
+      };
+      const result = await userCollection.insertOne(userToBeAdded);
+
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Get It Done is running");
+});
 
 app.listen(port, () => {
-    console.log(`Get It Done is running on port ${port}`)
-})
+  console.log(`Get It Done is running on port ${port}`);
+});
