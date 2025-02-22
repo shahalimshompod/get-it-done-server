f5fa9a27d7db2d4c8f4d1bb67411097d6374fded
shahalimshompod <shahalimsompod@gmail.com>
2025-02-22 18:27:30 +0600

    protected .env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "https://getitdone-24.web.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
