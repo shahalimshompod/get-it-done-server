165098ddcddcb653797f12ba1087326324510756
shahalimshompod <shahalimsompod@gmail.com>
2025-02-22 20:38:50 +0600

    fixed token related problem

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
